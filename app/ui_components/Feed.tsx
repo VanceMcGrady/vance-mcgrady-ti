2684f2cc0c85922279ea9e136d138c06b171a67e
VanceMcGrady <vance@virgalabs.io>
2024-05-23 15:29:39 -0700

    created FeedCard component

--- app/ui_components/Feed.tsx
+++ app/ui_components/Feed.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { getPostsForMe } from "../actions/getPostsForMe";
+import FeedCard from "./FeedCard";
 export default async function Feed() {
   const posts = await getPostsForMe();
   console.log("posts: ", posts);
   return (
     <div>
       {posts?.map((post) => {
-        return <p>{post.text}</p>;
+        return <FeedCard key={Math.random() * 10} />;
       })}
     </div>
   );
