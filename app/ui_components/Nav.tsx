f145753e39f4705b9fc1b729b392312ad79f1516
VanceMcGrady <vance@virgalabs.io>
2024-05-23 14:41:18 -0700

    added some basic styling and layout to navbar and two link elements

--- app/ui_components/Nav.tsx
+++ app/ui_components/Nav.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <div className="text-h-20 px-20 py-5 text-white flex justify-start align-center w-auto bg-blue-600  ">
+      <Link href={"/profile"}> Profile </Link>
+      <Link href={"/feed"}>Feed</Link>
+    </div>
+  );
 }
