0287144e8046963caec0666e5d08f86d532efd66
VanceMcGrady <vance@virgalabs.io>
2024-05-23 15:57:54 -0700

    display me data in Profile

--- app/ui_components/Profile.tsx
+++ app/ui_components/Profile.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { getMe } from "../actions/getMe";
 
-export default function Profile() {
-  return <div>Profile</div>;
+export default async function Profile() {
+  // ordinarily, this data would already be cached and present and we wouldn't have to refetch the data
+  // every time Profile renders.
+  const me = await getMe();
+
+  return (
+    <div>
+      <div>
+        <h3>{me?.name}</h3>
+        <p>bio: {me?.about}</p>
+        <p>Birdthday: {me?.birthday}</p>
+      </div>
+      <div>
+        <h3>Friends</h3>
+        {me?.friends.map((friend) => {
+          return <div key={Math.random() * 10}>{friend}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
