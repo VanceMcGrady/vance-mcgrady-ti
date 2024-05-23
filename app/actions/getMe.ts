2a3073834ca0bc9b9957aec9de35c4124702d9b8
VanceMcGrady <vance@virgalabs.io>
2024-05-23 15:12:41 -0700

    basic getMe mock api function

--- app/actions/getMe.ts
+++ app/actions/getMe.ts
@@ -1,5 +1,13 @@
 "use server";
 
+import { error } from "console";
+import { me } from "../mock_data/data";
 export default async function getMe() {
   // fetch my own user data
+  try {
+    return me;
+  } catch {
+    // send error to monitoring service of choice
+    console.log("error in getMe: ", error);
+  }
 }
