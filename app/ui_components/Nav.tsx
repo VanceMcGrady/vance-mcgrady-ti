7edd0c44b7ba7b8eebd3cf788052255774aa933c
VanceMcGrady <vance@virgalabs.io>
2024-05-23 14:48:33 -0700

    added some margins to links

--- app/ui_components/Nav.tsx
+++ app/ui_components/Nav.tsx
@@ -4,8 +4,13 @@ import Link from "next/link";
 export default function Nav() {
   return (
     <div className="text-h-20 px-20 py-5 text-white flex justify-start align-center w-auto bg-blue-600  ">
-      <Link href={"/profile"}> Profile </Link>
-      <Link href={"/feed"}>Feed</Link>
+      <Link href={"/profile"} className="mx-5">
+        {" "}
+        Profile{" "}
+      </Link>
+      <Link href={"/feed"} className="mx-5">
+        Feed
+      </Link>
     </div>
   );
 }
