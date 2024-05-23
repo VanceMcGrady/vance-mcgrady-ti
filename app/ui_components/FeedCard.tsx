179653204d61d21b39dca93bda1a759d0d306da1
VanceMcGrady <vance@virgalabs.io>
2024-05-23 15:50:09 -0700

    couple of styling tweaks

--- app/ui_components/FeedCard.tsx
+++ app/ui_components/FeedCard.tsx
@@ -11,9 +11,9 @@ export default function FeedCard({ post, key }: FeedCardProps) {
   return (
     <>
       <div className="flex flex-col w-full max-w-96 justify-center items-center my-2 px-2 py-5 border-2 border-indigo-50 rounded">
-        <h4>{post.user}</h4>
-        <p>{post.text}</p>
-        <div>
+        <h4 className="text-xl">{post.user}</h4>
+        <p className="m-1">{post.text}</p>
+        <div className="my-1">
           {detailView && (
             <div className="flex flex-row font-size-small gap-x-1">
               <p>Likes: {post.likes}</p>
