59a35aae504a6790cb5901bf2712dd23aee2ddf3
VanceMcGrady <vance@virgalabs.io>
2024-05-24 07:23:27 -0700

    update Nav to link to user profile dynamically

--- app/ui_components/Nav.tsx
+++ app/ui_components/Nav.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import { getUser } from "../actions/getUser";
+import { getMe } from "../actions/getMe";
 
-export default function Nav() {
+export default async function Nav() {
+  const me = await getMe();
   return (
     <div className="text-h-20 px-20 py-5 text-white flex justify-start align-center w-auto bg-blue-600  ">
-      <Link href={"/profile"} className="mx-5">
+      <Link href={`/profile/${me?.id}`} className="mx-5">
         {" "}
         Profile{" "}
       </Link>
