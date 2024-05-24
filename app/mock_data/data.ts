dc8f49f1ac785def341be0b509ca959dfa9f767f
VanceMcGrady <vance@virgalabs.io>
2024-05-24 07:10:40 -0700

    Update data.ts

--- app/mock_data/data.ts
+++ app/mock_data/data.ts
@@ -2,6 +2,7 @@
 export type Post = {
   id: number;
   user: string;
+  user_id: string;
   text: string;
   likes: number;
   date: string;
@@ -20,6 +21,7 @@ export const allPosts: Post[] = [
   {
     id: 1,
     user: "Tom",
+    user_id: "2",
     text: "feeling great today",
     likes: 50,
     time: "1pm",
@@ -28,6 +30,7 @@ export const allPosts: Post[] = [
   {
     id: 2,
     user: "Sarah",
+    user_id: "3",
     text: "excellent weather we're having",
     likes: 50,
     time: "1pm",
@@ -36,6 +39,7 @@ export const allPosts: Post[] = [
   {
     id: 3,
     user: "James",
+    user_id: "4",
     text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam magnam facere voluptates neque ex, nam saepe similique, fuga necessitatibus, animi perspiciatis odit! Commodi earum architecto autem natus. Itaque, temporibus distinctio?",
     likes: 50,
     time: "1pm",
@@ -44,6 +48,7 @@ export const allPosts: Post[] = [
   {
     id: 4,
     user: "Terry",
+    user_id: "5",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque excepturi nam repellat obcaecati aliquid quasi modi quisquam ut libero, blanditiis adipisci nobis ipsam debitis quam. Cumque reiciendis est quasi!",
     likes: 50,
     time: "1pm",
@@ -52,6 +57,7 @@ export const allPosts: Post[] = [
   {
     id: 5,
     user: "Steven",
+    user_id: "6",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque excepturi nam repellat obcaecati aliquid quasi modi quisquam ut libero, blanditiis adipisci nobis ipsam debitis quam. Cumque reiciendis est quasi!",
     likes: 50,
     time: "1pm",
@@ -60,6 +66,7 @@ export const allPosts: Post[] = [
   {
     id: 6,
     user: "Alice",
+    user_id: "7",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque excepturi nam repellat obcaecati aliquid quasi modi quisquam ut libero, blanditiis adipisci nobis ipsam debitis quam. Cumque reiciendis est quasi!",
     likes: 50,
     time: "1pm",
@@ -68,6 +75,7 @@ export const allPosts: Post[] = [
   {
     id: 7,
     user: "Ceaser",
+    user_id: "8",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque excepturi nam repellat obcaecati aliquid quasi modi quisquam ut libero, blanditiis adipisci nobis ipsam debitis quam. Cumque reiciendis est quasi!",
     likes: 50,
     time: "1pm",
@@ -76,6 +84,7 @@ export const allPosts: Post[] = [
   {
     id: 8,
     user: "John",
+    user_id: "9",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque excepturi nam repellat obcaecati aliquid quasi modi quisquam ut libero, blanditiis adipisci nobis ipsam debitis quam. Cumque reiciendis est quasi!",
     likes: 50,
     time: "1pm",
@@ -84,6 +93,7 @@ export const allPosts: Post[] = [
   {
     id: 9,
     user: "Meghan",
+    user_id: "10",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque excepturi nam repellat obcaecati aliquid quasi modi quisquam ut libero, blanditiis adipisci nobis ipsam debitis quam. Cumque reiciendis est quasi!",
     likes: 50,
     time: "1pm",
@@ -92,6 +102,7 @@ export const allPosts: Post[] = [
   {
     id: 10,
     user: "Spongebob",
+    user_id: "8",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque excepturi nam repellat obcaecati aliquid quasi modi quisquam ut libero, blanditiis adipisci nobis ipsam debitis quam. Cumque reiciendis est quasi!",
     likes: 50,
     time: "1pm",
@@ -106,3 +117,41 @@ export const me: User = {
   friends: ["Meghan", "Spongebob", "Steven", "Alice"],
   about: "This is a story about how my life got flip-turned upside-down",
 };
+
+export const users: User[] = [
+  {
+    id: 1,
+    name: "Vance",
+    birthday: "01/06/1991",
+    friends: ["Meghan", "Spongebob", "Steven", "Alice"],
+    about: "This is a story about how my life got flip-turned upside-down.",
+  },
+  {
+    id: 10,
+    name: "Meghan",
+    birthday: "05/11/1992",
+    friends: ["Vance", "Spongebob", "Steven", "Alice"],
+    about: "Sugar, we're going down swingin!",
+  },
+  {
+    id: 8,
+    name: "Spongebob",
+    birthday: "02/18/1990",
+    friends: ["Vance", "Spongebob", "Steven", "Alice"],
+    about: "If you're getting wet, you might as well swim. ",
+  },
+  {
+    id: 7,
+    name: "Alice",
+    birthday: "04/12/1969",
+    friends: ["Vance", "Spongebob", "Steven", "Alice"],
+    about: "How strange it is to be anything at all!",
+  },
+  {
+    id: 6,
+    name: "Steven",
+    birthday: "07/04/1989",
+    friends: ["Vance", "Spongebob", "Alice"],
+    about: "Hi, I'm Steven!",
+  },
+];
