6553b8706b43ee27daed4693c86fbe796e6e39ae
VanceMcGrady <vance@virgalabs.io>
2024-05-24 07:21:50 -0700

    update Profile to show data based on dynamic user

--- app/ui_components/Profile.tsx
+++ app/ui_components/Profile.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { getMe } from "../actions/getMe";
+import { getUser } from "../actions/getUser";
 
 export default async function Profile({ user_id }: { user_id: string }) {
   // ordinarily, this data would already be cached and present and we wouldn't have to refetch the data
   // every time Profile renders.
-  const me = await getMe();
+  const user = await getUser(user_id);
 
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center p-5 my-10 width-auto min-w-[80%] max-w-[80%] border-2 border-indigo-100 rounded">
-        <h3 className="text-xl">{me?.name}</h3>
-        <p>bio: {me?.about}</p>
-        <p>Birdthday: {me?.birthday}</p>
+        <h3 className="text-xl">{user?.name}</h3>
+        <p>bio: {user?.about}</p>
+        <p>Birdthday: {user?.birthday}</p>
       </div>
       <div className="flex flex-col justify-center items-center p-5 my-10 min-w-[80%]  max-w-[80%] border-2 border-indigo-100 rounded">
         <h3 className="text-xl ">Friends</h3>
-        {me?.friends.map((friend) => {
+        {user?.friends.map((friend) => {
           return <div key={Math.random() * 10}>{friend}</div>;
         })}
       </div>
